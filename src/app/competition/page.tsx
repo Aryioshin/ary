6daea24137a02e06aa3f91d2a4287a3d5125cfdc
crypto-cooker <mateo.kova0510@gmail.com>
2024-11-16 01:56:06 +0900

    fix:padding v

--- src/app/competition/page.tsx
+++ src/app/competition/page.tsx
@@ -17,7 +17,7 @@ export default function Page() {
     setTimeout(() => {
       load();
     }, 1000)
-    // setUserVolume(volumeSort(list));
+    // setUserVolume(volumeSort(list));1
   }, [])
   return (
     <div className="flex justify-center items-center w-full h-[100vh] text-green-200 pt-[300px]">
