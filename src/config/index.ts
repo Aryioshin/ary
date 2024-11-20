05da326c33513763b1e44960c3b2882bbc6aefbd
crypto-cooker <mateo.kova0510@gmail.com>
2024-11-21 00:28:09 +0900

    fix: update

--- src/config/index.ts
+++ src/config/index.ts
@@ -22,14 +22,6 @@ export const TOKEN_LIST = [
   { name: "TURTLE", isNative: false, address: "0x8C9E2bEf2962CE302ef578113eebEc62920B7e57", decimal: 18 },
 ]
 
-// export const TOKEN_LIST = [
-//   { name: "CRO", isNative: true, address: "", decimal: 18 },
-//   { name: "WCRO", isNative: false, address: "0x7b79995e5f793A07Bc00c21412e50Ecae098E7f9", decimal: 18 },
-//   { name: "ARY", isNative: false, address: "0x7C51fE4a0522f2b51BF35335F89E59A2b00e9851", decimal: 18 },
-//   { name: "USDC", isNative: false, address: "0x60A5a4efFd8E57125B957f91390597853F75662C", decimal: 18 },
-//   { name: "USDT", isNative: false, address: "0x6Eb2aB1cf0e5A8C9d62444816e75B095ccd55c98", decimal: 18 },
-//   { name: "MERY", isNative: false, address: "0x3b41B27E74Dd366CE27cB389dc7877D4e1516d4d", decimal: 18 },
-// ]
 
 export const WCRO = { name: "WCRO", isNative: false, address: "0x5C7F8A570d578ED84E63fdFA7b1eE72dEae1AE23", decimal: 18 }
  
